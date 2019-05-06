428639977bcf89151e91302f7abd1d6cd7c0789f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-05-06 10:37:53 +0900

    add pack.bat

---
+++ pack.bat
@@ -0,0 +1,9 @@
+set DIST=vista2xp-0.3
+if not exist %DIST% mkdir %DIST%
+copy README.txt %DIST%
+copy READMEJP.txt %DIST%
+copy LICENSE.txt %DIST%
+copy build\vista2xp.exe %DIST%
+copy build\v2xker32.dll %DIST%
+copy build\v2xctl32.dll %DIST%
+copy build\v2xu32.dll %DIST%
