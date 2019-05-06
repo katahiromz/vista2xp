571c49e614a23fd0162560ca919e1a1e0ca0df24
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-05-06 14:05:31 +0900

    improve taskdlg test

--- tests/taskdlg/taskdlg.c
+++ tests/taskdlg/taskdlg.c
@@ -12,22 +12,26 @@ int main(void)
     INT id;
     InitCommonControls();
 
-    TaskDialog(NULL, NULL, L"Test #1", L"Sheild", L"This is a test for TaskDialog.",
+    TaskDialog(NULL, NULL, L"Test #1", L"Sheild",
+               L"This is a test for TaskDialog.",
                TDCBF_YES_BUTTON | TDCBF_NO_BUTTON | TDCBF_CANCEL_BUTTON,
                TD_SHIELD_ICON, &id);
     printf("id: %d\n", id);
 
-    TaskDialog(NULL, NULL, L"Test #2", L"Error", L"This is a test for TaskDialog.",
+    TaskDialog(NULL, GetModuleHandleA(NULL), L"Test #2", L"Error",
+               MAKEINTRESOURCEW(100),
                TDCBF_YES_BUTTON | TDCBF_NO_BUTTON,
                TD_ERROR_ICON, &id);
     printf("id: %d\n", id);
 
-    TaskDialog(NULL, NULL, L"Test #3", L"Warning", L"This is a test for TaskDialog.",
+    TaskDialog(NULL, NULL, L"Test #3", L"Warning",
+               L"This is a test for TaskDialog.",
                TDCBF_RETRY_BUTTON | TDCBF_CANCEL_BUTTON,
                TD_WARNING_ICON, &id);
     printf("id: %d\n", id);
 
-    TaskDialog(NULL, NULL, L"Test #4", L"Information", L"This is a test for TaskDialog.",
+    TaskDialog(NULL, GetModuleHandleA(NULL), L"Test #4", MAKEINTRESOURCEW(101),
+               L"This is a test for TaskDialog.",
                TDCBF_OK_BUTTON | TDCBF_CANCEL_BUTTON,
                TD_INFORMATION_ICON, &id);
     printf("id: %d\n", id);
