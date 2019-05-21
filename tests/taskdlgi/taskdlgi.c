645f46b4126425ed598b82a42131401f1b83b0d1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-05-22 00:56:22 +0900

    improve taskdlgi

--- tests/taskdlgi/taskdlgi.c
+++ tests/taskdlgi/taskdlgi.c
@@ -227,6 +227,81 @@ int main(void)
             break;
         }
     }
+    {
+        TASKDIALOG_BUTTON buttons[] =
+        {
+            { 100, L"&Download and install the update now" },
+            { 101, L"Do &not download the update" },
+        };
+        BOOL bVerify = FALSE;
+        TASKDIALOGCONFIG config = { sizeof(TASKDIALOGCONFIG) };
+        config.hInstance = GetModuleHandleA(NULL);
+        config.pButtons = buttons;
+        config.cButtons = ARRAYSIZE(buttons);
+        config.pszWindowTitle = TEXT("WindowTitle");
+        config.pszMainIcon = TD_INFORMATION_ICON;
+        config.pszMainInstruction = TEXT("pszMainInstruction");
+        config.dwFlags = TDF_ALLOW_DIALOG_CANCELLATION | TDF_USE_COMMAND_LINKS;
+        config.pszVerificationText = TEXT("pszVerificationText");
+        INT selected = 0xDEADBEEF;
+        HRESULT hr = TaskDialogIndirect(&config, &selected, NULL, &bVerify);
+        printf("hr: %08X\n", hr);
+        printf("selected: %d\n", selected);
+    }
+    {
+        TASKDIALOG_BUTTON buttons[] =
+        {
+            { FINE, TEXT("&Fine") },
+            { CLOUD, TEXT("&Cloud") },
+            { RAIN, TEXT("&Rain") },
+            { SNOW, TEXT("&Snow") }
+        };
+        TASKDIALOG_BUTTON radio_buttons[] =
+        {
+            { 1000, TEXT("Radio 1") },
+            { 1001, TEXT("Radio 2") },
+            { 1002, TEXT("Radio 3") },
+        };
+        TASKDIALOGCONFIG config;
+        ZeroMemory(&config, sizeof(config));
+        config.cbSize = sizeof(config); 
+        config.hInstance = GetModuleHandleA(NULL);
+        config.hwndParent = NULL;
+        config.pButtons = buttons;
+        config.cButtons = ARRAYSIZE(buttons);
+        config.pRadioButtons = radio_buttons;
+        config.cRadioButtons = ARRAYSIZE(radio_buttons);
+        config.dwFlags = TDF_ALLOW_DIALOG_CANCELLATION | TDF_USE_COMMAND_LINKS | TDF_NO_DEFAULT_RADIO_BUTTON;
+        config.pszMainIcon = TD_WARNING_ICON; 
+        config.pszWindowTitle = TEXT("with radio buttons 3");
+        config.pszMainInstruction = TEXT("pszMainInstruction"); 
+        config.pszContent = TEXT("pszContent"); 
+        config.pszFooterIcon = TD_INFORMATION_ICON;
+        config.pszFooter = TEXT("pszFooter");
+        config.nDefaultButton = CLOUD;
+        config.pszVerificationText = TEXT("pszVerificationText\nTESTTEST\nTEST");
+        INT selected = 0xDEADBEEF;
+        INT radio = 0xDEADBEEF;
+        BOOL bVerify = FALSE;
+        HRESULT hr = TaskDialogIndirect(&config, &selected, &radio, &bVerify);
+        printf("hr: %08X\n", hr);
+        printf("radio: %08X\n", radio);
+        switch (selected)
+        {
+        case FINE:
+            puts("FINE");
+            break;
+        case CLOUD:
+            puts("CLOUD");
+            break;
+        case RAIN:
+            puts("RAIN");
+            break;
+        case SNOW:
+            puts("SNOW");
+            break;
+        }
+    }
 #endif
 
     return 0;
