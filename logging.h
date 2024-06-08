611c6533aa95dc20f40ac8030980f8341297110b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-06-08 18:33:18 +0900

    MLOG_REGKEY

--- logging.h
+++ logging.h
@@ -1,4 +1,5 @@
 #define MLOG_CODEPAGE CP_ACP
 #define MLOG_USE_STRSAFE
 #define MLOG_FILE_OUTPUT TEXT("%APPDATA%\\vista2xp.log")
+#define MLOG_REGKEY TEXT("Software\\Katayama Hirofumi MZ\\vista2xp")
 #include "MLog/MLog.h"
